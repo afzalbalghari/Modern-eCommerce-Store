447dbace0564b87a9c47b0b835790d3f08c3f478
muhammadafzal <mafzalbalghari101@gmail.com>
2026-03-28 01:19:26 -0700

    fix bugs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AppProvider } from "./context/AppContext";
-import Navbar   from "./components/Navbar";
+import Navbar   from "/components/Navbar";
 import Footer   from "./components/Footer";
 import Home     from "./pages/Home";
 
