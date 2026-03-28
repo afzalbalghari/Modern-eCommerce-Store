f3eb0f63f76313c2032b4ccc9d6f0e62e608b90d
muhammadafzal <mafzalbalghari101@gmail.com>
2026-03-28 06:25:57 -0700

    import the product, productDetails & card page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AppProvider } from "./context/AppContext";
-import Navbar   from "/components/Navbar";
+import Navbar   from "./components/Navbar";
 import Footer   from "./components/Footer";
 import Home     from "./pages/Home";
-
+import Products from "./pages/Products";
+import ProductDetails from "./pages/ProductDetails";
+import Cart     from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Login    from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
 
 export default function App() {
   return (
@@ -14,7 +20,13 @@ export default function App() {
           <main className="flex-1">
             <Routes>
               <Route path="/"              element={<Home />} />
-              
+              <Route path="/products"      element={<Products />} />
+              <Route path="/product/:id"   element={<ProductDetails />} />
+              <Route path="/cart"          element={<Cart />} />
+              <Route path="/checkout"      element={<Checkout />} />
+              <Route path="/login"         element={<Login />} />
+              <Route path="/register"      element={<Register />} />
+              <Route path="/dashboard"     element={<Dashboard />} />
             </Routes>
           </main>
           <Footer />
