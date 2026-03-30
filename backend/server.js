fd3ad97bbb6ee41e9e077fbfefec2f6ab493a011
muhammadafzal <mafzalbalghari101@gmail.com>
2026-03-30 04:25:47 -0700

    Basic Server Setup

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+// server.js
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// Routes
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/products", require("./routes/productRoutes"));
+app.use("/api/orders", require("./routes/orderRoutes"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
